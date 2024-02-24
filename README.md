fbfc02eecd2c20dd18173e19bce2594c30ebd1fa
Rupesh Ghosh <rupeshghosh10@gmail.com>
2024-02-24 23:02:03 +0530

    add csharpier for formating

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # chat-app-authentication
+
+## Usage
+Restore husky and csharpier and setup husky
+```
+dotnet tool restore
+dotnet husky install
+```
--- chat-app-authentication/Program.cs
+++ chat-app-authentication/Program.cs
@@ -3,7 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,9 +16,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
